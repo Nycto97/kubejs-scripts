fc04d87be63938883101c4644b7373252eb039ba
Nycto97 <jelle.van.goethem@hotmail.com>
2024-02-01 06:42:48 +0100

    Remove mod loaded check for JEI and ProjectE events
    KubeJS only fires these events if the mod is loaded, so no need to check it again

--- client_scripts/jei.js
+++ client_scripts/jei.js
@@ -6,52 +6,45 @@
    LICENSE file in the root directory of this source tree.
 */
 
-if (Platform.isLoaded('jei')) {
-    /* Hide items in JEI interface */
-    JEIEvents.hideItems((event) => {
-        /* All vertical slabs from Builders Crafts and
-           Additions since Quark already adds these */
-        if (Platform.isLoaded('buildersaddition')) event.hide(/buildersaddition:.*vertical_slab/);
+/* Hide items in JEI interface */
+JEIEvents.hideItems((event) => {
+    /* All vertical slabs from Builders Crafts and
+       Additions since Quark already adds these */
+    if (Platform.isLoaded('buildersaddition')) event.hide(/buildersaddition:.*vertical_slab/);
 
-        /* All vertical slabs from Vertical Slabs Compat - Create: Deco
-           since Create: Deco already adds these */
-        if (Platform.isLoaded('v_slab_compat') && Platform.isLoaded('createdeco'))
-            event.hide(/v_slab_compat:createdeco.*vertical_slab/);
+    /* All vertical slabs from Vertical Slabs Compat - Create: Deco
+       since Create: Deco already adds these */
+    if (Platform.isLoaded('v_slab_compat') && Platform.isLoaded('createdeco'))
+        event.hide(/v_slab_compat:createdeco.*vertical_slab/);
 
-        /* Vertical slab from Double Slabs since Quark already adds this
-           and this currently is uncraftable anyway and just acts
-           as air when mining double slab vertical slabs when this
-           is enabled in Double Slabs config (currently disabled) */
-        if (Platform.isLoaded('doubleslabs')) event.hide('doubleslabs:vertical_slab');
+    /* Vertical slab from Double Slabs since Quark already adds this
+       and this currently is uncraftable anyway and just acts
+       as air when mining double slab vertical slabs when this
+       is enabled in Double Slabs config (currently disabled) */
+    if (Platform.isLoaded('doubleslabs')) event.hide('doubleslabs:vertical_slab');
 
-        if (Platform.isLoaded('morecraft')) {
-            /* All emerald armor from MoreCraft since
-               Useless Swords already adds these */
-            event.hide([
-                'morecraft:emerald_helmet',
-                'morecraft:emerald_chestplate',
-                'morecraft:emerald_leggings',
-                'morecraft:emerald_boots'
-            ]);
+    if (Platform.isLoaded('morecraft')) {
+        /* All emerald armor from MoreCraft since
+           Useless Swords already adds these */
+        event.hide([
+            'morecraft:emerald_helmet',
+            'morecraft:emerald_chestplate',
+            'morecraft:emerald_leggings',
+            'morecraft:emerald_boots'
+        ]);
 
-            /* Ruby gems, blocks and ores from MoreCraft
-               since we use Epic Samurai's ruby */
-            event.hide([
-                'morecraft:ruby',
-                'morecraft:ruby_block',
-                'morecraft:ruby_ore',
-                'morecraft:deepslate_ruby_ore'
-            ]);
-        }
+        /* Ruby gems, blocks and ores from MoreCraft
+           since we use Epic Samurai's ruby */
+        event.hide(['morecraft:ruby', 'morecraft:ruby_block', 'morecraft:ruby_ore', 'morecraft:deepslate_ruby_ore']);
+    }
 
-        /* Disabled items from Paraglider */
-        if (Platform.isLoaded('paraglider'))
-            event.hide([
-                'paraglider:heart_container',
-                'paraglider:stamina_vessel',
-                'paraglider:spirit_orb',
-                'paraglider:anti_vessel',
-                'paraglider:essence'
-            ]);
-    });
-}
+    /* Disabled items from Paraglider */
+    if (Platform.isLoaded('paraglider'))
+        event.hide([
+            'paraglider:heart_container',
+            'paraglider:stamina_vessel',
+            'paraglider:spirit_orb',
+            'paraglider:anti_vessel',
+            'paraglider:essence'
+        ]);
+});
--- server_scripts/projecte.js
+++ server_scripts/projecte.js
@@ -6,24 +6,22 @@
    LICENSE file in the root directory of this source tree.
 */
 
-if (Platform.isLoaded('projecte')) {
-    ProjectEEvents.setEMC((event) => {
-        /* Sets the absolute EMC value of an item. */
-        // event.setEMC('minecraft:cobblestone', 0); // alias. setEMCAfter
+ProjectEEvents.setEMC((event) => {
+    /* Sets the absolute EMC value of an item. */
+    // event.setEMC('minecraft:cobblestone', 0); // alias. setEMCAfter
 
-        /* Sets the EMC of an item before anything else happens.
-           This can sometimes result in this EMC value not
-           being set, but also it allows for EMC values
-           to be generated from this one; i.e crafting recipes */
-        if (Platform.isLoaded('architects_palette')) {
-            event.setEMCBefore('architects_palette:withered_bone', 160);
-        }
+    /* Sets the EMC of an item before anything else happens.
+       This can sometimes result in this EMC value not
+       being set, but also it allows for EMC values
+       to be generated from this one; i.e crafting recipes */
+    if (Platform.isLoaded('architects_palette')) {
+        event.setEMCBefore('architects_palette:withered_bone', 160);
+    }
 
-        if (Platform.isLoaded('blue_skies')) {
-            event.setEMCBefore('blue_skies:lunar_cobblestone', 1);
-        }
+    if (Platform.isLoaded('blue_skies')) {
+        event.setEMCBefore('blue_skies:lunar_cobblestone', 1);
+    }
 
-        /* Prevent XP exploit */
-        event.setEMCBefore('minecraft:experience_bottle', 0);
-    });
-}
+    /* Prevent XP exploit */
+    event.setEMCBefore('minecraft:experience_bottle', 0);
+});
