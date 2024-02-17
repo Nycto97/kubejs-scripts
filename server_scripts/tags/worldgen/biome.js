fc74f0384e0e1659f952115d6c689283b85ada48
Nycto97 <jelle.van.goethem@hotmail.com>
2024-02-17 08:58:28 +0100

    Update license comments

--- server_scripts/tags/worldgen/biome.js
+++ server_scripts/tags/worldgen/biome.js
@@ -1,10 +1,12 @@
-/*
-   Copyright (c) 2023, Jelle Van Goethem
-   All rights reserved.
- 
-   This source code is licensed under the BSD-style license found in the
-   LICENSE file in the root directory of this source tree.
-*/
+/**
+ * @license BSD-3-Clause
+ *
+ * Copyright (c) 2023, Jelle Van Goethem
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 ServerEvents.tags('worldgen/biome', (event) => {
     /* Rotten Creatures */
